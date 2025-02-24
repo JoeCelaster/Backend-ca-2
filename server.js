9e3152656a8e76ffc6cb43fb0770c68e2fc125f8
JoeCelaster <joecelaster2006@gmail.com>
2025-02-24 14:17:16 +0530

    CAA_2

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express() 
+const PORT = 5000
+const users = []
+
+app.use(express.json())
+
+app.post('/users',(req,res)=>{
+    const {username,email,password,Dateofbirth} = req.body
+    const newName = {username,email,password,Dateofbirth}
+
+    if (!username){
+        res.status(400).send("Username cannot be empty")
+    }
+    if (!email){
+        res.status(400).send("Email cannot be empty")
+    }
+    if (password.length < 8 || password.length >16){
+        res.status(400).send("Password length should be greater than 8 or less than or equal to 16")
+    }
+    users.push(newName)
+    res.status(200).send(users)
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is running at http://localhost:5000")
+})
